551d8f6668814d9a1d08c32654f3ea510fc94ff8
aithusaqr <aisha@aithusa.ca>
2022-12-27 15:17:35 -0500

    regenerated sql scripts after enabling emit_empty_slices in sqlc.yaml

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -12,7 +12,7 @@ import (
 const addAccountBalance = `-- name: AddAccountBalance :one
 UPDATE accounts
 SET balance = balance + $1
-WHERE id  =$2
+WHERE id  = $2
 RETURNING id, owner, balance, currency, created_at
 `
 
@@ -127,7 +127,7 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 		return nil, err
 	}
 	defer rows.Close()
-	var items []Account
+	items := []Account{}
 	for rows.Next() {
 		var i Account
 		if err := rows.Scan(
