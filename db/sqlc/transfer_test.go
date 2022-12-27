7e08e89c6880d3ca943b70760790bd07c003e8ae
aithusaqr <aisha@aithusa.ca>
2022-12-27 15:19:45 -0500

    updated test

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -34,11 +34,11 @@ func TestCreateTransfer(t *testing.T) {
 	createRandomTransfer(t)
 }
 
-func TestUpdateTransfer(t *testing.T){
+func TestUpdateTransfer(t *testing.T) {
 	transfer1 := createRandomTransfer(t)
 
 	arg := UpdateTransferParams{
-		ID: transfer1.ID,
+		ID:     transfer1.ID,
 		Amount: util.RandomMoney(),
 	}
 
@@ -55,32 +55,31 @@ func TestUpdateTransfer(t *testing.T){
 	require.NotZero(t, transfer2.CreatedAt)
 }
 
-
-func TestDeleteTransfer(t *testing.T){
+func TestDeleteTransfer(t *testing.T) {
 	transfer1 := createRandomTransfer(t)
 
 	err := testQueries.DeleteTransfer(context.Background(), transfer1.ID)
 
 	require.NoError(t, err)
 
-	transfer2, err:= testQueries.GetTransfer(context.Background(), transfer1.ID)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
 
 	require.Error(t, err)
 	require.EqualError(t, err, sql.ErrNoRows.Error())
 	require.Empty(t, transfer2)
 }
 
-func TestListTransfers(t *testing.T){
-	for i := 0; i < 5; i++{
+func TestListTransfers(t *testing.T) {
+	for i := 0; i < 10; i++ {
 		createRandomTransfer(t)
 	}
 
 	arg := ListTransfersParams{
-		Limit: 5,
+		Limit:  5,
 		Offset: 5,
 	}
 
-	transfers, err:= testQueries.ListTransfers(context.Background(), arg)
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
 	require.NoError(t, err)
 	require.Len(t, transfers, 5)
 
