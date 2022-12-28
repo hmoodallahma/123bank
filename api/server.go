580b8df8879d93169f1006a8bb2eaaef71f0efe6
aithusaqr <aisha@aithusa.ca>
2022-12-28 12:54:15 -0500

    added custom validator for currency

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/hmoodallahma/123bank/db/sqlc"
 )
 
@@ -16,6 +18,10 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	// add validators
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 	// add routes to router
 	router.POST("/account", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
--- api/validator.go
+++ api/validator.go
@@ -1,5 +1,14 @@
 package api
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/hmoodallahma/123bank/util"
+)
 
-var validCurrency validator.Func
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		// check currency supported
+		return util.IsSupportedCurrency(currency)
+	}
+	return false
+}
