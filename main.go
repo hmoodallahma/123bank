17027ca9e048eac1a29ca786136739a48810542d
aithusaqr <aisha@aithusa.ca>
2023-01-17 12:31:08 -0500

    revert deletion error

--- main.go
+++ main.go
@@ -92,6 +92,9 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	http.Handle("/swagger/", http.StripPrefix("/swagger", fs))
+
 	listener, err := net.Listen("tcp", config.HttpServerAddress)
 
 	if err != nil {
