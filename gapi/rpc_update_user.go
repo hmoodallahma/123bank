62be2688dafad4daddd16a8058c6071f13a5eabd
aithusaqr <aisha@aithusa.ca>
2023-01-23 13:16:07 -0500

    implimented rpc_update_user

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,100 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	db "github.com/hmoodallahma/123bank/db/sqlc"
+	"github.com/hmoodallahma/123bank/pb"
+	"github.com/hmoodallahma/123bank/util"
+	"github.com/hmoodallahma/123bank/val"
+	"github.com/lib/pq"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+)
+
+func (server *Server) UpdateUser(ctx context.Context,
+	req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	arg := db.UpdateUserParams{
+		Username: req.GetUsername(),
+		FullName: sql.NullString{
+			String: req.GetFullName(),
+			Valid:  req.FullName != nil,
+		},
+		Email: sql.NullString{
+			String: req.GetEmail(),
+			Valid:  req.Email != nil,
+		},
+	}
+	if req.Password != nil {
+		hashedPassword, err := util.HashPassword(req.GetPassword())
+
+		if err != nil {
+			return nil, status.Errorf(codes.Internal,
+				"error hashing password: %s", err)
+		}
+		arg.HashedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+		arg.PasswordChangedAt = sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		}
+	}
+
+	// todo: refactor --> encapsulate to a util function
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists,
+					"username already exists, %s", err)
+			}
+		}
+		return nil, status.Errorf(codes.Internal,
+			"failed to create user: %s", err)
+	}
+	res := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	// todo: set res.User.CreatedAt
+	// todo: set res.User.PasswordUpdatedAt
+	return res, nil
+
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (
+	violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.Password != nil {
+		if err := val.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	if req.FullName != nil {
+		if err := val.ValidateFullName(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := val.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	return violations
+}
